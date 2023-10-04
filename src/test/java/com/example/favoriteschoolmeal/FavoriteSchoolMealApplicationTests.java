a65cedb873827af9c3daa203fa093549af32aac1
Ahn HyungJun <ahj3579@naver.com>
2023-10-04 16:08:48 +0900

    first commit

---
+++ src/test/java/com/example/favoriteschoolmeal/FavoriteSchoolMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.favoriteschoolmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteSchoolMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
