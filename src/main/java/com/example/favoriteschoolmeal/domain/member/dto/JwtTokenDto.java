4cde913cf93cdf2f5e2030b5c3f006cc44782bf3
Ahn HyungJun <ahj3579@naver.com>
2023-11-11 03:36:11 +0900

    feat: Implemented RefreshTokenService

---
+++ src/main/java/com/example/favoriteschoolmeal/domain/member/dto/JwtTokenDto.java
@@ -0,0 +1,18 @@
+package com.example.favoriteschoolmeal.domain.member.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class JwtTokenDto {
+    private String accessToken;
+    private String refreshToken;
+}
