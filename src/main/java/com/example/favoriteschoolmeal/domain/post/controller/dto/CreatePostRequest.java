3a71b216c9fb37b6536d9655be90ea7c2e70ad6c
J0YERIM <chosarah70@gmail.com>
2023-11-11 23:59:35 +0900

    refactor: CreatePostRequest, CreatePostCommand에 유효성 검증 어노테이션 추가

--- src/main/java/com/example/favoriteschoolmeal/domain/post/controller/dto/CreatePostRequest.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/post/controller/dto/CreatePostRequest.java
@@ -1,7 +1,12 @@
 package com.example.favoriteschoolmeal.domain.post.controller.dto;
 
-// TODO: 의존성 추가 후 @NotNull, @NotBlank 어노테이션 추가
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CreatePostRequest(
-        Long memberId, Long matchingId, String title, String content) {
+        @NotNull Long memberId,
+        @NotNull Long matchingId,
+        @NotBlank String title,
+        @NotBlank String content) {
 
 }
--- src/main/java/com/example/favoriteschoolmeal/domain/post/service/dto/CreatePostCommand.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/post/service/dto/CreatePostCommand.java
@@ -4,10 +4,14 @@ import com.example.favoriteschoolmeal.domain.matching.domain.Matching;
 import com.example.favoriteschoolmeal.domain.member.domain.Member;
 import com.example.favoriteschoolmeal.domain.post.controller.dto.CreatePostRequest;
 import com.example.favoriteschoolmeal.domain.post.domain.Post;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-// TODO: 의존성 추가 후 @NotNull, @NotBlank 어노테이션 추가
 public record CreatePostCommand(
-        Long memberId, Long matchingId, String title, String content) {
+        @NotNull Long memberId,
+        @NotNull Long matchingId,
+        @NotBlank String title,
+        @NotBlank String content) {
 
     public static CreatePostCommand from(final CreatePostRequest createPostRequest) {
         return new CreatePostCommand(
