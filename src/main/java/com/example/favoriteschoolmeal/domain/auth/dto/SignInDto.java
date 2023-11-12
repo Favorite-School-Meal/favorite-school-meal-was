1430c35b11a0680bb68136758bddc5a9699db718
Ahn HyungJun <ahj3579@naver.com>
2023-11-12 23:34:29 +0900

    feat: Implemented SignUpDto

--- src/main/java/com/example/favoriteschoolmeal/domain/auth/dto/SignInDto.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/auth/dto/SignInDto.java
@@ -1,7 +1,17 @@
 package com.example.favoriteschoolmeal.domain.auth.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public class SignInDto {
+public record SignInDto(
+
+        @NotBlank
+        String username,
+
+        @NotBlank
+        @Size(min = 8, max = 16, message = "비밀번호의 길이는 8 ~ 16자입니다")
+        String password
+){
 
 }
--- src/main/java/com/example/favoriteschoolmeal/domain/auth/dto/SignUpDto.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/auth/dto/SignUpDto.java
@@ -1,20 +1,34 @@
 package com.example.favoriteschoolmeal.domain.auth.dto;
 
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-public class SignUpDto {
+public record SignUpDto(
 
-    @NotBlank
-    private String username;
+        @NotBlank
+        @Size(min = 3, max = 20, message = "아이디는 3자리 이상, 20자리 이하여야 합니다.")
+        String username,
 
-    private String fullname;
+        @NotBlank
+        @Size(min = 2, max = 4, message = "이름은 2자리 이상, 4자리 이하여야 합니다.")
+        String fullname,
 
-    private String nickname;
+        @NotBlank
+        @Size(min = 3, max = 20, message = "닉네임은 3자리 이상, 20자리 이하여야 합니다.")
+        String nickname,
 
-    private String email;
+        @NotBlank
+        @Email(message = "email 형식이여야 합니다.")
+        String email,
 
-    private String password;
+        @NotBlank
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 길이, 영문 대소문자, 숫자, 특수문자를 모두 포함해야 합니다.")
+        String password
+) {
+
 
 
 }
