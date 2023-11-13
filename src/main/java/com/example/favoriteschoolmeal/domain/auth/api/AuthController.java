5491d0f4336d80c439f51201064b25f22b0a0d13
Ahn HyungJun <ahj3579@naver.com>
2023-11-14 02:26:45 +0900

    fix: Fixed jwt validity error and Implemented reCreateToken api

--- src/main/java/com/example/favoriteschoolmeal/domain/auth/api/AuthController.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/auth/api/AuthController.java
@@ -8,11 +8,10 @@ import com.example.favoriteschoolmeal.global.common.response.ApiResponse;
 import com.example.favoriteschoolmeal.global.security.jwt.JwtTokenProvider;
 import com.example.favoriteschoolmeal.global.security.token.refresh.RefreshTokenServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -32,6 +31,7 @@ public class AuthController {
     public ApiResponse<JwtTokenDto> signUp(@RequestBody SignUpRequest signUpRequest){
         JwtTokenDto jwtTokenDto = authService.signUp(signUpRequest);
 
+        log.info("유저가 생성되었습니다. {}", signUpRequest.username());
         return ApiResponse.createSuccess(jwtTokenDto);
     }
 
@@ -44,8 +44,31 @@ public class AuthController {
     public ApiResponse<JwtTokenDto> signIn(@RequestBody SignInRequest signInRequest){
         JwtTokenDto jwtTokenDto = authService.signIn(signInRequest);
 
+        log.info("유저가 로그인 되었습니다. {}", signInRequest.username());
         return ApiResponse.createSuccess(jwtTokenDto);
     }
 
+    /**
+     * RefreshToken을 사용하여 새로운 AccessToken과 RefreshToken을 발급하는 메서드
+     * @param refreshToken 사용할 RefreshToken
+     * @return 발급된 새로운 AccessToken과 RefreshToken에 대한 TokenDto 객체
+     */
+    @PostMapping("/refresh-token")
+    public ApiResponse<JwtTokenDto> reCreateToken(@RequestHeader("Authorization") String refreshToken){
+
+        String newAccessToken = refreshTokenService.reCreateAccessTokenByRefreshToken(refreshToken);
+        String newRefreshToken = refreshTokenService.reCreateRefreshTokenByRefreshToken(refreshToken);
+
+        JwtTokenDto jwtTokenDto = JwtTokenDto.builder()
+                                .accessToken(newAccessToken)
+                                .refreshToken(newRefreshToken)
+                                .build();
+
+        refreshToken = refreshToken.substring(7);
+        refreshTokenService.createRefreshToken(jwtTokenDto, jwtTokenProvider.getUsername(refreshToken));
+
+        log.info("토큰이 재생성 되었습니다. {}", jwtTokenProvider.getUsername(refreshToken) );
+        return ApiResponse.createSuccess(jwtTokenDto);
+    }
 
 }
