7e213fa9730688829aa7b57ca3dd292b10d4cbbd
Yeom <ydh7219@naver.com>
2023-11-15 19:52:25 +0900

    feat: ChatService 생성

---
+++ src/main/java/com/example/favoriteschoolmeal/domain/chat/service/ChatService.java
@@ -0,0 +1,15 @@
+package com.example.favoriteschoolmeal.domain.chat.service;
+
+import com.example.favoriteschoolmeal.domain.chat.domain.Chat;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChatService {
+
+    public Optional<Chat> findChatById(Long chatId) {
+        //TODO: 구현 필요
+        return Optional.empty();
+    }
+}
--- src/main/java/com/example/favoriteschoolmeal/domain/report/service/ReportService.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/report/service/ReportService.java
@@ -1,11 +1,11 @@
 package com.example.favoriteschoolmeal.domain.report.service;
 
 import com.example.favoriteschoolmeal.domain.chat.domain.Chat;
+import com.example.favoriteschoolmeal.domain.chat.service.ChatService;
 import com.example.favoriteschoolmeal.domain.comment.domain.Comment;
 import com.example.favoriteschoolmeal.domain.comment.service.CommentService;
 import com.example.favoriteschoolmeal.domain.member.domain.Member;
 import com.example.favoriteschoolmeal.domain.member.service.MemberService;
-import com.example.favoriteschoolmeal.domain.model.ReportType;
 import com.example.favoriteschoolmeal.domain.post.domain.Post;
 import com.example.favoriteschoolmeal.domain.post.service.PostService;
 import com.example.favoriteschoolmeal.domain.report.controller.dto.CreateReportRequest;
@@ -29,6 +29,7 @@ public class ReportService {
     private final PostService postService;
     private final CommentService commentService;
     private final MemberService memberService;
+    private final ChatService chatService;
 
     public ReportResponse addReport(CreateReportRequest request, CustomUserDetails userDetails) {
         Member reporter = userDetails.getMember();
@@ -50,14 +51,15 @@ public class ReportService {
 
     }
 
-    private static Report buildChatReport(CreateReportRequest request, Member reporter) {
-        //TODO: chat 구현 필요
-        //            Chat chat = chatService.findChatById(request.chatId())
-        //                    .orElseThrow(() -> new ReportCreationException(ReportExceptionType.CHAT_NOT_FOUND));
-        Chat chat = null; //임시
+    private Report buildChatReport(CreateReportRequest request, Member reporter) {
+        Chat chat = chatService.findChatById(request.chatId())
+                .orElseThrow(() -> new ReportCreationException(ReportExceptionType.CHAT_NOT_FOUND));
+
+        Member reportedMember = memberService.findMemberById(request.reportedMemberId())
+                .orElseThrow(() -> new ReportCreationException(ReportExceptionType.MEMBER_NOT_FOUND));
         return Report.builder()
                 .reporter(reporter)
-                //                    .reportedMember(chat.getMember())
+                .reportedMember(reportedMember)
                 .reportedChat(chat)
                 .reportType(request.reportType())
                 .content(request.content())
@@ -65,7 +67,6 @@ public class ReportService {
     }
 
     private Report buildCommentReport(CreateReportRequest request, Member reporter) {
-        //TODO: comment 구현 필요
         Comment comment = commentService.findCommentById(request.commentId())
                 .orElseThrow(() -> new ReportCreationException(ReportExceptionType.COMMENT_NOT_FOUND));
         return Report.builder()
@@ -90,7 +91,6 @@ public class ReportService {
     }
 
     private Report buildPostReport(CreateReportRequest request, Member reporter) {
-        //TODO: postService.findPostById 구현 필요
         Post post = postService.findPostById(request.postId())
                 .orElseThrow(() -> new ReportCreationException(ReportExceptionType.POST_NOT_FOUND));
 
