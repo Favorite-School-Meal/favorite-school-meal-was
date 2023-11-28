c14935c3151e0e3cb8f8dd47454be371be98cf8e
Ahn HyungJun <ahj3579@naver.com>
2023-11-29 03:30:53 +0900

    fix: memberSimpleResponse username 누락 복구

--- src/main/java/com/example/favoriteschoolmeal/domain/member/dto/MemberSimpleResponse.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/member/dto/MemberSimpleResponse.java
@@ -7,12 +7,14 @@ import java.util.Optional;
 
 public record MemberSimpleResponse(
         String nickname,
+        String username,
         String profileImageEndpoint) {
 
     public static MemberSimpleResponse from(final Member member) {
 
         return new MemberSimpleResponse(
                 member.getNickname(),
+                member.getUsername(),
                 Optional.ofNullable(member.getProfileImage())
                         .map(FileEntity::getEndpoint)
                         .orElse(null));
