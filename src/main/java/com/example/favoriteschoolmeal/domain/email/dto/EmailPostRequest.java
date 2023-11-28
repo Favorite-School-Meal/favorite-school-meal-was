10a5dde65e96e1ae577164b00dc3df0425d026b7
Ahn HyungJun <ahj3579@naver.com>
2023-11-29 02:56:41 +0900

    feat: Request record 필드 어노테이션 추가

--- src/main/java/com/example/favoriteschoolmeal/domain/email/dto/EmailPostRequest.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/email/dto/EmailPostRequest.java
@@ -1,7 +1,16 @@
 package com.example.favoriteschoolmeal.domain.email.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record EmailPostRequest(
+        @NotBlank
+        @Email(message = "email 형식이여야 합니다.")
         String email,
+
+        @NotBlank
+        @Size(min = 3, max = 20, message = "아이디는 3자리 이상, 20자리 이하여야 합니다.")
         String username
 ) {
 }
--- src/main/java/com/example/favoriteschoolmeal/domain/member/dto/FindUsernameRequest.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/member/dto/FindUsernameRequest.java
@@ -1,10 +1,17 @@
 package com.example.favoriteschoolmeal.domain.member.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record FindUsernameRequest(
 
-        @NotBlank String fullname,
-        @NotBlank String email
+        @NotBlank
+        @Size(min = 2, max = 4)
+        String fullname,
+
+        @NotBlank
+        @Email(message = "email 형식이여야 합니다.")
+        String email
 ) {
 }
