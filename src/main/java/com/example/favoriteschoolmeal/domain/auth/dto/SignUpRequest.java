7ff165f706258e6e2f13fada77c69a3d7687ae29
Ahn HyungJun <ahj3579@naver.com>
2023-11-29 22:19:47 +0900

    fix: signUpRequest password 패턴 메세지 수정

--- src/main/java/com/example/favoriteschoolmeal/domain/auth/dto/SignUpRequest.java
+++ src/main/java/com/example/favoriteschoolmeal/domain/auth/dto/SignUpRequest.java
@@ -25,7 +25,7 @@ public record SignUpRequest(
         String email,
 
         @NotBlank
-        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 길이, 햐")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 길이, 영문 대소문자, 숫자, 특수문자를 모두 포함해야 합니다.")
         String password,
 
         @NotBlank
